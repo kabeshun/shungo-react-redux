d02f6a916422e9c9e66a1348254bb08a4a471edd
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-06-26 18:26:59 +0900

    delete all geolocation implemention

--- src/index.js
+++ src/index.js
@@ -1,50 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
-import Spinner from "./Spinner";
 class App extends React.Component {
-  //initializing state
-  constructor(props) {
-    super(props);
-
-    //ここだけ、直接代入できる。ほかではuseStateを使う
-    this.state = { lat: null, errorMessage: "" };
-  }
-
-  state = { lat: null, errorMessage: "" };
-
-  //データの取得系はここがいい
-  componentDidMount() {
-    console.log("did mount");
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-        //we called setstate!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (error) => {
-        console.error(error);
-        this.setState({ errorMessage: error.message });
-      }
-    );
-  }
-
-  componentDidUpdate() {
-    console.log("did update");
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay latitude={this.state.lat} />;
-    } else {
-      return <Spinner message="Please accept location request" />;
-    }
-  }
-
   render() {
-    return <div className="border red">{this.renderContent()}</div>;
+    return <div>New Proj</div>;
   }
 }
 
