09fe9fd0dca562f9c9a96620e6fdb9c59a6cabc6
Shungo Kusakabe <shungo@Shungos-MacBook-Pro.local>
2021-06-26 13:11:31 +0900

    use React.component in order to use constructor, state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+class App extends React.Component {
+  //initializing state
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return <div>Season</div>;
-};
+    //ここだけ、直接代入できる。ほかではuseStateを使う
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        //we called setstate!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
