726a52be5e4c1c03c6f41bc5e2486a685a8a1c51
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-06-26 18:22:26 +0900

    mode place of claass

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,20 @@
 import "./SeasonDisplay.css";
 import React from "react";
 
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.latitude, new Date().getMonth());
+  const text = seasonConfig[season].text;
+  const iconName = seasonConfig[season].iconName;
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`}></i>
+      <h1>Season: {text}</h1>
+      <i className={`${iconName} icon massive icon-right`}></i>
+    </div>
+  );
+};
+
 const seasonConfig = {
   summer: {
     text: "Lets hit the beach",
@@ -19,20 +33,6 @@ const getSeason = (lat, month) => {
   } else {
     return lat > 0 ? "winter" : "summer";
   }
-};
-
-const SeasonDisplay = (props) => {
-  const season = getSeason(props.latitude, new Date().getMonth());
-  const text = seasonConfig[season].text;
-  const iconName = seasonConfig[season].iconName;
-
-  return (
-    <div className={`season-display ${season}`}>
-      <i className={`${iconName} icon massive icon-left`}></i>
-      <h1>Season: {text}</h1>
-      <i className={`${iconName} icon massive icon-right`}></i>
-    </div>
-  );
 };
 
 export default SeasonDisplay;
