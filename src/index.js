ff8d7e6f9d7f1ce516bb61c8775e31caa42c6495
Shungo Kusakabe <shungo@Shungos-MacBook-Pro.local>
2021-06-26 12:16:36 +0900

    delete all works

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import faker from "faker";
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45pm"
-        text="Hi I am Sam"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:45pm"
-        text="Hi I am Alex"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:45pm"
-        text="Hi I am Jane"
-        avatar={faker.image.avatar()}
-      />
-    </div>
-  );
+  return <div>Season</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
