f8dc32d8e277b5657fb6a0dc3ab5c761c990f11d
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-06-26 23:13:40 +0900

    fetch data and show length of images

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import ImageList from "./components/ImageList/ImageList";
 import axios from "axios";
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term,
@@ -14,12 +16,14 @@ class App extends React.Component {
       },
     });
 
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+    console.log(this.state);
+  };
   render() {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
         <ImageList />
       </div>
     );
