b0b7c94ef4c4d9408eac76458105b10feac5ce49
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-06-26 23:07:18 +0900

    npm install axios, use async await to fetch data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImageList from "./components/ImageList/ImageList";
+import axios from "axios";
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log("term");
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID VzcxpX0orYbEF-NGXx5kZEExvo7eLd2RQ6SVQIbSpvg",
+      },
+    });
+
+    console.log(response.data.results);
   }
   render() {
     return (
