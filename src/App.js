8578b6b10830b1ee16622a5533de12e6fcc0e938
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-06-28 19:47:27 +0900

    add search bar components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends React.Component {
   render() {
-    return <div className="ui container">index</div>;
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
   }
 }
 
