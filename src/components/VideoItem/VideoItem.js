debab3083d55c16210c9713193ae94b20aba0f02
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-06-28 20:31:31 +0900

    render videos

--- src/components/VideoItem/VideoItem.js
+++ src/components/VideoItem/VideoItem.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const VideoItem = (props) => {
-  return <div>Video Item</div>;
+  console.log(props.video.snippet.title);
+  return (
+    <div>
+      <img src={props.video.snippet.thumbnails.medium.url} />
+      {props.video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
