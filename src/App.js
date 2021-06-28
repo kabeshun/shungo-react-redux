9851f332330fe96020992e34e6303ee8316274f2
Shungo Kusakabe <shungo12tennis@gmail.com>
2021-06-28 20:21:28 +0900

    Video list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
 import youtube from "./apis/youtube";
+import VideoList from "./components/VideoList.js/VideoList";
 
 class App extends React.Component {
   state = { videos: [] };
   onTermSubmit = async (term) => {
-    console.log(term);
     const response = await youtube.get("/search", {
       params: {
         q: term,
@@ -18,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
-        {this.state.videos.length}
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/VideoList.js/VideoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoList = (props) => {
+  return <div>VideoList {props.videos.length}</div>;
+};
+
+export default VideoList;
