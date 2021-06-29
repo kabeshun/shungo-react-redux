eb7f67ca8c16c63509410c6571e87bc21cdbe69c
Shungo Kusakabe <shungo@Shungos-MacBook-Pro.local>
2021-06-29 23:42:46 +0900

    fetch data from wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    //Get from Wikipedia
+    if (!term) return;
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      if (!!data.query) {
+        setResults(data.query.search);
+      }
+    };
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+        </div>
+        {result.snippet}
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -14,6 +50,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
