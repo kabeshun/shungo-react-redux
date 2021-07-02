14bc44a6cb7c25beaff3f7a1ea3c5e2c8bfd7832
kabeshun <shuno12tennis@gmail.com>
2021-07-02 21:43:06 +0900

    dropdown implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
 
 const App = () => {
-  return <Search />;
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <Dropdown
+      options={options}
+      selected={selected}
+      onSelectedChange={setSelected}
+    />
+  );
 };
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
 
 export default App;
